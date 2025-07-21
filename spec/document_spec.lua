97724c89a0069af3deb74b47f4e0415ad0031ae1
Michal Hoftich <michal.h21@gmail.com>
2025-07-21 21:57:24 +0200

    reading yaml header works

--- spec/document_spec.lua
+++ spec/document_spec.lua
@@ -1,11 +1,42 @@
 expose("Test document", function()
     local document = require "moonscribe.document"
-    describe("some assertions", function()
-        it("tests positive assertions", function()
-            assert.is_true(true)    -- Lua keyword chained with _
-            assert.True(true)         -- Lua keyword using a capital
-            assert.are.equal(1, 1)
-            assert.has.errors(function() error("this should fail") end)
+    describe("parse preamble", function()
+        it("should return the whole text if there was no preamble", function()
+          local texts = { "hello world",  "hello\nworld", "--- no closing dashes"}
+          for _, text in ipairs(texts) do
+            local metadata = document.parse_content(text)
+            assert.are.same({ content = text }, metadata)
+          end
+        end)
+        it("should parse preamble and content", function()
+          local text1 = [[---
+title: Hello title
+---
+Hello world
+]]
+
+          local text2 = [[
+---
+title: "Hello title"
+---
+Hello world
+]]
+
+          for _, text in ipairs({ text1, text2 }) do
+            local metadata = document.parse_content(text)
+            assert.are.same({
+              title = "Hello title",
+              content = "Hello world\n"
+            }, metadata)
+          end
+        end)
+        it("should parse dates", function()
+          local origdate =  "2025-07-20"
+          local text = string.format("---\ntest: %s\n---\nxxx", origdate)
+          local metadata = document.parse_content(text)
+          -- the yaml library parses dates as Lua timestamps and substracts 8 hours, I don't know why
+          -- but we can fix it by adding 8 hours
+          assert.same(os.date("%Y-%m-%d", metadata.test + 8 * 3600), origdate)
         end)
     end)
 end)
--- src/moonscribe/document.lua
+++ src/moonscribe/document.lua
@@ -1,11 +1,81 @@
 local lfs = require "lfs"
+local yaml = require "yaml"
 
-local function parse_content(text)
+--- read a line from the text starting at position
+---@param text string
+---@param position number
+---@return string
+---@return number
+local function read_line(text, position)
+  local start, stop = text:find("\n", position)
+  if not start then return nil, position end
+  local line = text:sub(position, start - 1)
+  return line, stop + 1
 end
 
 
+--- Check if the line starts with three dashes
+--- @param text string
+--- @return boolean
+local function match_dashes(text)
+  return text:match("^%s*%-%-%-")
+end
 
---- @param
+local function table_join(tbl1, tbl2) 
+  for k,v in pairs(tbl2) do
+    tbl1[k] = v
+  end
+  return tbl1
+end
+
+--- Parse the frontmatter from the text, return the frontmatter and the rest of the text
+--- If there is no frontmatter, return nil and the whole text as content
+---@param text string
+---@return string
+---@return string
+local function parse_frontmatter(text)
+  local frontmatter = {}
+  if not match_dashes then
+    return nil, text -- No frontmatter found, return the whole text as content
+  end
+  local line, stop = read_line(text, 1)
+  -- this can happen if there are no line breaks
+  if not line or not match_dashes(line) then
+    return nil, text -- No frontmatter found, return the whole text as content
+  end
+  local found_second_dash = false
+  while line do
+    line, stop = read_line(text, stop)
+    if match_dashes(line) then
+      found_second_dash = true
+      break -- End of frontmatter
+    end
+    frontmatter[#frontmatter + 1] = line
+  end
+  if not found_second_dash then
+    return nil, text -- No closing dashes found, return the whole text as content
+  end
+  local metadata = yaml.eval(table.concat(frontmatter, "\n"))
+  return metadata, text:sub(stop)
+end
+
+--- Parse the document for the pramble and text content, return metadata table
+---@param text string
+---@return table
+local function parse_content(text)
+  local metadata = {}
+  local frontmatter, content = parse_frontmatter(text)
+  if frontmatter then
+    metadata = table_join(metadata, frontmatter)
+  end
+  metadata.content = content
+  return metadata
+end
+
+
+--- comment
+--- @param path string
+--- @return table
 local function load_file(path)
   local attributes = lfs.attributes(path)
   local f = io.open(path, "r")
